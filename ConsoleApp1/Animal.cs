75779fec118f05e96356db2afed70857133c3d3f
Asus <Asus@ARSHIA>
2023-10-09 22:07:02 +0330

    Add project files.

---
+++ ConsoleApp1/Animal.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Hand { get; set; }
+        public int Feet { get; set; }
+   
+        public Animal(string name, int hand,int feet)
+        {
+
+            Name = name;
+            Hand = hand;
+            Feet = feet;
+        
+          
+        }
+        public void Makesound()
+        {
+
+            Console.WriteLine("the animal makes a sound:");
+        }
+
+
+        public void Animaldetails()
+        {
+            Console.WriteLine("Animal Name:" + Name);
+            Console.WriteLine("number of hands:" + Hand);
+            Console.WriteLine("number of feet:" + Feet);
+         
+        }
+
+    }
+    public class Cow:Animal
+    {
+        public Cow(string name , int hand , int feet): base(name, hand ,feet)
+        {
+            
+        }
+
+        public  void Makesound()
+        {
+
+            Console.WriteLine("moo!moo!");
+        }
+        public void Animaldetails()
+        {
+            Console.WriteLine("Animal Name:" + Name);
+            Console.WriteLine("number of hands:" + Hand);
+            Console.WriteLine("number of feet:" + Feet);
+
+
+        }
+
+    }
+    public class Cat:Animal
+    {
+        public Cat(string name , int hand , int feet): base(name, hand ,feet) 
+        {
+         
+        }
+
+       
+
+        public void Makesound()
+        {
+            Console.WriteLine("meows");
+        }
+        public void Animaldetails()
+        {
+
+            Console.WriteLine("Animal Name:" + Name);
+            Console.WriteLine("number of hands:" + Hand);
+            Console.WriteLine("number of feet:" + Feet);
+
+        }
+
+
+    }
+    public class Fly:Animal
+    {
+        public Fly(string name , int hand ,int feet):base(name,hand,feet) 
+        { 
+        
+        
+        }
+        public void Makesound()
+        {
+            Console.WriteLine("zzzzzz!");
+
+
+        }
+
+        public void Animaldetails()
+        {
+            Console.WriteLine("Animal Name:" + Name);
+            Console.WriteLine("number of hands:" + Hand);
+            Console.WriteLine("number of feet:" + Feet);
+
+
+
+        }
+    }
+   
+
+
+
+}
+   
+    
+    public class Handssection
+    {
+        public int Count { get; set; }
+
+        public Handssection()
+        {
+
+            Count = 0;
+        }
+
+
+    }
+    public class Feetsection
+    {
+        public int Count { get; set; }
+        public Feetsection()
+        {
+            Count = 0;
+        }
+
+    }
+   
+    
+
+
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+
+Animal animal = new Animal("snake",0,0);
+animal.Makesound();
+animal.Animaldetails();
+
+Console.WriteLine();
+Cat cat = new Cat("cat", 2, 2);
+cat.Animaldetails();
+cat.Makesound();
+
+Console.WriteLine();
+
+Cow cow = new Cow("cow", 2, 2);
+cow.Animaldetails();
+cow.Makesound();
+Console.WriteLine();
+
+Fly fly = new Fly("fly", 2, 4);
+fly.Animaldetails();
+fly.Makesound();
+Console.WriteLine();
